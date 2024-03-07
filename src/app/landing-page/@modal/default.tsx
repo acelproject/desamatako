3f0f6b94e30f6f9bacfe428c65642af78573ec6a
acelproject <aclprcjt@gmail.com>
2024-03-07 10:49:06 +0800

    fix erorr deploy 7/3/2024

--- src/app/landing-page/@modal/default.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Default = () => {
-  return null;
-};
-
-export default Default;
--- src/app/landing-page/detail-links/[id]/page.tsx
+++ src/app/landing-page/detail-links/[id]/page.tsx
@@ -1,11 +1,11 @@
-import Modal from "@/components/layouts/modals";
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
 import React, { MouseEventHandler, useRef } from "react";
 import link1 from "../../../../../public/assets/img/garuda.png";
 import link2 from "../../../../../public/assets/img/kemendes.png";
 import link3 from "../../../../../public/assets/img/kemenparekraf.png";
-import { useRouter } from "next/navigation";
 type DetailLinksProps = {
   params: number;
   linksDetail: boolean;
@@ -15,7 +15,6 @@ const LinksDetail = (props: DetailLinksProps) => {
   const { params, linksDetail, setLinksDetail } = props;
 
   const overlay = useRef(null);
-  const router = useRouter();
 
   const close: MouseEventHandler = (e) => {
     if (e.target === overlay.current) {
