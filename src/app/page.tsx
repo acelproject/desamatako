87ac90d7a1b8e23381284c1fea9232fa80c000f5
acelproject <aclprcjt@gmail.com>
2024-03-07 12:19:01 +0800

    fix erorr deploy 7/3/2024 13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import AppSheel from "@/components/layouts/AppSheel/page";
-
 import Navbar from "./navbar";
 import Nav from "./navbar";
 import Link from "next/link";
@@ -9,13 +7,8 @@ import { IoIosArrowUp } from "react-icons/io";
 import { useEffect, useState } from "react";
 import LandingPage from "./landing-page/page";
 
-
-
-
 export default function Home() {
-
   const [onScroll, setOnScroll] = useState(false);
-
 
   const toTop = () => {
     if (window.scrollY >= 1) {
@@ -28,22 +21,20 @@ export default function Home() {
   useEffect(() => {
     window.addEventListener("scroll", toTop);
   });
-  
+
   return (
     <div>
-      {/* <AppSheel>
-      </AppSheel> */}
-        <LandingPage/>
-        <div>
-          <Link
-            href="#top"
-            className={`p-3 bg-primary rounded-full fixed m-5 bottom-0 right-0 z-[999] text-white shadow-md transition-all duration-150 delay-150 ${
-              onScroll ? "scale-100" : "scale-0"
-            }`}
-          >
-            <IoIosArrowUp />
-          </Link>
-        </div>
+      <LandingPage />
+      <div>
+        <Link
+          href="#top"
+          className={`p-3 bg-primary rounded-full fixed m-5 bottom-0 right-0 z-[999] text-white shadow-md transition-all duration-150 delay-150 ${
+            onScroll ? "scale-100" : "scale-0"
+          }`}
+        >
+          <IoIosArrowUp />
+        </Link>
+      </div>
     </div>
   );
 }
