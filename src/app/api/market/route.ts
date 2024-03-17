13a1821b2910fc94de5c55d4e320d3cbdac695a5
acelproject <aclprcjt@gmail.com>
2024-03-17 12:41:02 +0800

    editedddd

--- src/app/api/market/route.ts
+++ src/app/api/market/route.ts
@@ -1,44 +1,6 @@
 import { retrieveData, retrieveDataById } from "@/lib/firebase/service";
 import { NextResponse, NextRequest } from "next/server";
 
-// const data = [
-//   {
-//     id: 1,
-//     nama: "Durian",
-//     category: "buah",
-//     penjual: "Bapak Marchel",
-//     jumlah: 10,
-//     harga: 90000,
-//     img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/13190eab-1f63-421e-b8da-c96e5d08ab50/air-jordan-1-low-se-craft-shoes-76v7Vm.png",
-//   },
-//   {
-//     id: 2,
-//     nama: "Mesin Paras",
-//     category: "bangunan",
-//     penjual: "Bapak Andre",
-//     jumlah: 2,
-//     harga: 200000,
-//     img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b5406b0d-fb0b-4e94-ab81-4d24a3d79a2f/air-force-1-07-shoes-8jXZH2.png",
-//   },
-//   {
-//     id: 3,
-//     nama: "Mesin Paras",
-//     category: "bangunan",
-//     penjual: "Bapak Andre",
-//     jumlah: 2,
-//     harga: 200000,
-//     img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b5406b0d-fb0b-4e94-ab81-4d24a3d79a2f/air-force-1-07-shoes-8jXZH2.png",
-//   },
-//   {
-//     id: 4,
-//     nama: "Mesin Paras",
-//     category: "bangunan",
-//     penjual: "Bapak Andre",
-//     jumlah: 2,
-//     harga: 200000,
-//     img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b5406b0d-fb0b-4e94-ab81-4d24a3d79a2f/air-force-1-07-shoes-8jXZH2.png",
-//   },
-// ];
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,4 +1,3 @@
-// import getData from "@/services/market/indeks";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,22 +11,19 @@ export default async function MarketPage() {
   const products = await getDocs();
   console.log(products);
   return (
-    // api luar
-    // <div className=" flex gap-5 bg-primary flex-wrap">
-    //   {products.length > 0 &&
-    //     products.map((product: any) => (
-    //       <div key={product.id} className="w-40">
-    //         <h1 className="truncate">{product.title}</h1>
-    //       </div>
-    //     ))}
-    // </div>
-
     // api sendiri
     <div className=" flex gap-5 bg-white flex-wrap w-10/12 justify-center mx-auto">
       {products.data.length > 0 &&
         products.data.map((product: any, i: any) => (
           <Link href={`market/detail/${product.id}`} key={i}>
-            <Image src={product.img} alt="" width={300} height={200} priority loading="lazy"/>
+            <Image
+              src={product.img}
+              alt=""
+              width={300}
+              height={200}
+              priority
+              loading="lazy"
+            />
             <div>nama: {product.nama}</div>
             <div>Kategori: {product.category}</div>
             <div className="text-3xl">Kategori: {product.harga}</div>
