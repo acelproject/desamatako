b414aca100da920f0115287862d5063d673fa480
acelproject <aclprcjt@gmail.com>
2024-03-17 12:51:01 +0800

    editedddd 2

--- src/app/market/detail/[...slug]/page.tsx
+++ src/app/market/detail/[...slug]/page.tsx
@@ -1,4 +1,4 @@
-import getData from "@/services/market/indeks";
+// import getData from "@/services/market/indeks";
 import Image from "next/image";
 
 type DetailMarketProps = {
@@ -6,20 +6,20 @@ type DetailMarketProps = {
 };
 
 export default async function DetailMarketPage(props: DetailMarketProps) {
-  const { params } = props;
+  // const { params } = props;
 
-  const data = await getData(
-    `http://localhost:3000/api/market/?id=${
-      params.slug[params.slug.length - 1]
-    }`
-  );
+  // const data = await getData(
+  //   `http://localhost:3000/api/market/?id=${
+  //     params.slug[params.slug.length - 1]
+  //   }`
+  // );
   return (
     <div>
       <h1>Detail Market Place</h1>
-
+{/* 
       <div>
         <Image src={data.data.img} alt="" width={300} height={100} />
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,19 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-
-const getDocs = async () => {
-  const res = await fetch("http://localhost:3000/api/market", {
-    cache: "no-cache",
-  });
-  return res.json();
-};
 export default async function MarketPage() {
-  const products = await getDocs();
-  console.log(products);
   return (
     // api sendiri
     <div className=" flex gap-5 bg-white flex-wrap w-10/12 justify-center mx-auto">
-      {products.data.length > 0 &&
+      {/* {products.data.length > 0 &&
         products.data.map((product: any, i: any) => (
           <Link href={`market/detail/${product.id}`} key={i}>
             <Image
@@ -28,7 +17,8 @@ export default async function MarketPage() {
             <div>Kategori: {product.category}</div>
             <div className="text-3xl">Kategori: {product.harga}</div>
           </Link>
-        ))}
+        ))} */}
+        building
     </div>
   );
 }
