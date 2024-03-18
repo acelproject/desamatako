917fe691b2bfdc1eae4b64d7bda377094cb070cf
acelproject <aclprcjt@gmail.com>
2024-03-18 19:13:28 +0800

    edited 18/03/24 2

--- src/app/market/products/page.tsx
+++ src/app/market/products/page.tsx
@@ -1,44 +1,21 @@
-"use client";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 import useSWR from "swr";
-// async function getData() {
-//   const res = await fetch("http://localhost:3000/api/products", {
-//     cache: "no-store",
-//     // next: {
-//     //   // revalidate: 10
-//     //   tags: ["products"],
-//     // },
-//   });
-
-//   if (!res.ok) {
-//     throw new Error("gagal memuat data");
-//   }
-
-//   return res.json();
-// }
 
 const fetcher = (url: string) =>
-  fetch(url, { cache: "no-store" }).then((res) => {
+  fetch(url).then((res) => {
     return res.json();
   });
-
-export default function MarketPage() {
-  // const products = await getData();
+export default function page() {
   const { data, error, isLoading } = useSWR(
     `${process.env.NEXT_PUBLIC_API_URL}/api/products`,
     fetcher
   );
-
-  const products = {
-    data: data.data,
-  };
-
   return (
-    // api sendiri
     <div className=" flex gap-5 bg-white flex-wrap w-10/12 justify-center mx-auto">
-      {products.data.length > 0 &&
-        products.data.map((product: any, i: any) => (
+      {data.data.length > 0 &&
+        data.data.map((product: any, i: any) => (
           <Link href={`market/detail/${product.id}`} key={i}>
             <Image src={product.img} alt="" width={300} height={200} priority />
             <div>nama: {product.nama}</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import AppWrapper from "./AppWrapper";
 import { Metadata } from "next";
+import { URL } from "url";
+// import { URL } from "next/dist/compiled/@edge-runtime/primitives/url";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/"),
   title: "Landing Page Desa Matako",
   description:
     "Website Pemerintah Desa Matako, Kabupaten Tojo una-una, Kecamatan Tojo Barat.",
